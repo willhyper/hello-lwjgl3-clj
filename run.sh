d1fa6c71a1781126519f752653ced955594d0ffa
willhyper <willhyper@gmail.com>
2023-10-07 09:53:57 -0700

    adapt change for macos

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-clj -XstartOnFirstThread -M raw-opengl-lwjgl3.clj 
+# https://libgdx.com/wiki/app/starter-classes-and-configuration#common-issues
+# https://stackoverflow.com/questions/70024246/lwjgl-error-glfw-may-only-be-used-on-the-main-thread-this-check-may-be-disable?rq=3
+
+# https://clojure.org/reference/deps_and_cli
+
+#https://ask.clojure.org/index.php/10674/could-tools-alpha-support-opts-aliases-data-paths-extra-paths
+
+clj -Mmacos raw-opengl-lwjgl3.clj 
