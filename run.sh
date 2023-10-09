79e2e84d6406f162ffedf5abd7ee53a06b8a7396
willhyper <willhyper@gmail.com>
2023-10-09 15:34:36 -0700

    remove ref links

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 # https://libgdx.com/wiki/app/starter-classes-and-configuration#common-issues
-# https://stackoverflow.com/questions/70024246/lwjgl-error-glfw-may-only-be-used-on-the-main-thread-this-check-may-be-disable?rq=3
 
-# https://clojure.org/reference/deps_and_cli
 
 #https://ask.clojure.org/index.php/10674/could-tools-alpha-support-opts-aliases-data-paths-extra-paths
 
